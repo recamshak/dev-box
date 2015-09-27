19aa28a1718600a1ec78075fbdfbf793ebcdf08a
Joël Billaud <jbillaud@gmail.com>
2015-09-28 06:49:46 +0900

    first working draft

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-#
-# Setup the the box. This runs as root
 
-apt-get -y update
-
-apt-get -y install curl
-
-# You can install anything you need here.
+dnf -y install facter
---
+++ scripts/i3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+dnf -y install i3 lightdm lightdm-gtk
+#systemctl disable gdm
+#systemctl enable lightdm
+
